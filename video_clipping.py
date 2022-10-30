04b84de247e4d64e2697f1215a7e26fdfe862aba
Acnes25 <tigerzouxk@gmail.com>
2022-10-30 10:24:09 -0400

    Update video_clipping.py

--- video_clipping.py
+++ video_clipping.py
@@ -1,6 +1,10 @@
 from pyyoutube import Api
 from yt_dlp import YoutubeDL
+import yt_dlp
 
+help(yt_dlp)
+"""
 URLS = ['https://www.youtube.com/watch?v=BaW_jenozKc']
 with YoutubeDL() as ydl:
     ydl.download(URLS)
+"""
