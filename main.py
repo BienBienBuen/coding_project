374ac163919d8a38c335e984a7ba620a87d4bfae
BienBienBuen <33363816+BienBienBuen@users.noreply.github.com>
2022-10-30 21:21:59 +0800

    pysh

--- main.py
+++ main.py
@@ -32,16 +32,19 @@ def get_channel_info(c_id):
     Channel_info = Channel_by_id.items[0].to_dict()
     return Channel_info
 
-def get_upload(c_id):
+def get_upload(c_id, number):
     info = get_channel_info(c_id)
     playlist_id = info["contentDetails"]["relatedPlaylists"]["uploads"]
     upload_list = api.get_playlist_items(playlist_id = playlist_id)
-    return upload_list.items[0].to_dict()
+    videoID = []
+    for i in range (number):
+        videoID.append(upload_list.items[i].to_dict()["contentDetails"]["videoId"])
+    return videoID
 
 
 
 channel_info = get_channel_info(channel_id)
 print(channel_info)
-print(get_upload(channel_id))
+print(get_upload(channel_id, 5))
 # video_by_chart = api.get_videos_by_chart(chart="mostPopular", region_code="US", count=2)
 
